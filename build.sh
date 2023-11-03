a929d388c20fa187fe90494ef0a3f00550bc5d84
suhaib0110 <alromys@gmail.com>
2023-11-03 17:05:01 +0200

    delete createsu.py and edite build.sh3

--- build.sh
+++ build.sh
@@ -4,7 +4,5 @@ set -o errexit  # exit on error
 
 pip install -r requirements.txt
 
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
+python manage.py createsuperuser --no-input
 fi
