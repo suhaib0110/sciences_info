cfc2b871bb6f4275f1c66615fd6296c8a34994e6
suhaib0110 <alromys@gmail.com>
2023-11-03 16:50:37 +0200

    delete createsu.py and edite build.sh

--- build.sh
+++ build.sh
@@ -2,24 +2,11 @@
 
 set -o errexit  # exit on error
 
-from django.contrib.auth.models import User
-from django.core.management.base import BaseCommand
-
-
-class Command(BaseCommand):
-help = 'Creates a superuser.'
-
-def handle(self, *args, **options):
-if not User.objects.filter(username='alromy').exists():
-    User.objects.create_superuser(
-        username='alromy',
-        password='3s77t00p@ertert77'
-    )
-print('Superuser has been created.')
-
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
-python manage.py migrate
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
 
+  print('Superuser has been created.')
 fi
