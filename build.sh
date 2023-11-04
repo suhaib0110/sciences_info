5777660a2fe5dc3d4791b4ce321da75571717e69
suhaib0110 <alromys@gmail.com>
2023-11-04 18:39:53 +0200

    build.sh:poetry install, add render.yaml

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@
 
 set -o errexit  # exit on error
 
-pip install -r requirements.txt
+poetry install
+
+python manage.py collectstatic --no-input
+python manage.py migrate
+
+#pip install -r requirements.txt
--- sciences/settings.py
+++ sciences/settings.py
@@ -69,6 +69,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', ####render host for static
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -152,7 +153,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL    =  '/media/'
 MEDIA_ROOT  =   BASE_DIR/'mediafiles'
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #TKEditar:
 
